ee93eefc822801ae670a1b1ccaf15d5d7b25a9b8
gioiascari <gioia.ascari96@gmail.com>
2022-05-18 19:11:34 +0200

    finita pag senza array però

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,135 @@
-
 <footer>
+    <div class="bg-footer">
 
+        <div class="container-footer">
+            <div class="container-nav-footer">
+                <div class="columns">
+                    <!-- Dc Comics Nav -->
+                    <nav>
+                        <h4>Dc Comics</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+                            <li>
+                                <a href="#">Tv</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>                
+                        </ul>
+                    </nav>
+    
+                    <!-- Shop Nav -->
+                    <nav>
+                        <h4>Shop</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Shop Dc</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Dc Collectibles</a>
+                            </li>  
+                        </ul>
+                    </nav>
+                </div>
+    
+                <div class="colums">
+    
+                    <!-- Dc Nav -->
+                    <nav>
+                        <h4>Dc</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Term Of Use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy policy (New)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+    
+                <div class="columns">
+                    <!-- Sites Nav -->
+                    <nav>
+                        <h4>Sites</h4>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">MAD Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC kinds</a>
+                            </li>  
+                            <li>
+                                <a href="#">DC Universe</a>
+                            </li>  
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="bottom-footer">
+            <div class="bottom-footer-container">
+                <button class="btn btn-footer">Sign-up Now!</button>
+                <div class="d-flex">
+                    <div class="social">
+                        <h4>Follow Us</h4>
+                        <img src="{{ asset('img/footer-facebook.png') }}" alt="facebook">
+                        <img src="{{ asset('img/footer-twitter.png') }}" alt="twitter">
+                        <img src="{{ asset('img/footer-youtube.png') }}" alt="youtube">
+                        <img src="{{ asset('img/footer-pinterest.png') }}" alt="pinterest">
+                        <img src="{{ asset('img/footer-periscope.png') }}" alt="periscope">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
