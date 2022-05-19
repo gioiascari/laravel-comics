83aaa496f8f0c9ebbb8ce4c5ad7bbc4b253e8aa0
gioiascari <gioia.ascari96@gmail.com>
2022-05-19 20:41:01 +0200

    inserimento dati card al click, da sistemare il layout

--- resources/views/partials/single.blade.php
+++ resources/views/partials/single.blade.php
@@ -1,23 +1,39 @@
 
- @include('layouts.app')
+@extends('layouts.app')
 
 
-{{-- @section('main') --}}
-<section>
+ @section('main')
+ {{-- Card top focus --}}
+<div class="singleCard">
+    <div class="cards-container">
+        <div class="card">
+            <a href="/">Go Back</a>
+            <img src="{{$comic['thumb']}}" alt="">
+        </div>
+     </div>
+</div>
+ {{--/ Card top focus --}}
+ {{-- Card text focus --}}
+ <div class="">
+     <div class="title-container">
+        <h1>{{$comic['title']}}</h1>
+     </div>
+     <div class="availability">
+         <p>U.S Price : {{$comic['price']}}</p>
+         <p>Available</p>
+     </div>
+     <div class="description">
+        <img src="{{$comic['thumb']}}" alt="">
 
-    <h1></h1>
-    <a href="/">
+         <p>
+             {{$comic['description']}}
+         </p>
+     </div>
 
-
-        <p>{{$comic['series']}}</p>
-
-    </a>
-
-    {{ $comic['title'] }}
+ </div>
+ {{--/ Card text focus --}}
 
 
 
 
-</section>
-
-{{-- @endsection --}}
+@endsection
