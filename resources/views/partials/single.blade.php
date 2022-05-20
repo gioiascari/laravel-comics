1b49642d71ee3222e47b03c773c254aa215a3cb5
gioiascari <gioia.ascari96@gmail.com>
2022-05-20 22:12:23 +0200

    sistemato layout single card con sass

--- resources/views/partials/single.blade.php
+++ resources/views/partials/single.blade.php
@@ -8,41 +8,52 @@
 <div class="singleCard">
     <div class="cards-container">
         <div class="card">
-            <a href="/">Go Back</a>
             <img src="{{$comic['thumb']}}" alt="">
         </div>
      </div>
 </div>
  {{--/ Card top focus --}}
  {{-- Card text focus --}}
- <div class="c-main justify-center">
-    <div class="title-container">
-        <h1>{{$comic['title']}}</h1>
-     </div>
- </div>
- <div class="c-main">
-     <div class="availability">
-         <div class="text-desc">
-            <p>U.S Price : {{$comic['price']}}</p>
-        </div>
-        <div class="text-desc">
-            <p>Available</p>
-        </div>
 
-     </div>
- </div>
+
+
  <div class="c-main">
+
+
     <div class="description">
+
         <div class="text-desc">
-            <p>
-                {{$comic['description']}}
-            </p>
+            <div class="title-container p-10">
+                <h1>{{$comic['title']}}</h1>
+             </div>
+             <div class="availability">
+                 <div class="text-desc ">
+                    <p><span>U.S Price : </span>{{$comic['price']}}</p>
+                </div>
+                <div class="text-desc flex-end">
+                    <span>Available</span>
+                    <p>
+                        <a href="/">
+                            Check Availability
+                        </a>
+
+                    </p>
+                </div>
+
+
+             </div>
+             <div class="p-10">
+                <p>
+                    {{$comic['description']}}
+                </p>
+             </div>
         </div>
         <img src="{{$comic['thumb']}}" alt="">
      </div>
  </div>
+</div>
   {{--/TALENT SECTION --}}
-    </div>
+
   {{--/ Card text focus --}}
 
 
